535a752cbb09f6629105d06cad4e7df74a185ba2
Isaac Smith <isaac.smitty@gmail.com>
2019-02-14 23:08:19 +0000

    added userid pair to urldb object, added function to check that urls belong to user, fixed edit and delete routes to work with updated DB

--- express_server.js
+++ express_server.js
@@ -13,12 +13,21 @@ const PORT = 8080
 app.set('view engine', 'ejs');
 
 const urlDB = {
-  'b2xVn2': 'http://www.lighthouselabs.ca',
-  '9sm5xK': 'http://www.google.com',
-  'p98nb5': 'http://www.downtowndecks.com',
-  '23cb6v': 'http://helm.life'
+  'b2xVn2': {longURL: 'http://www.lighthouselabs.ca', userID: 'f74b36'},
+  '9sm5xK': {longURL: 'http://www.google.com', userID: 'gu6g6f'},
+  'p98nb5': {longURL: 'http://www.downtowndecks.com', userID: 'gu6g6f'},
+  '23cb6v': {longURL: 'http://helm.life', userID: 'f74b36'}
 };
 
+function urlsForUser(id) {
+  let accessableURLs = {};
+  for (let shortURL in urlDB) {
+    if (urlDB[shortURL].userID === id) {
+      accessableURLs[shortURL] = urlDB[shortURL].longURL;
+    }
+  }
+  return accessableURLs;
+}
 const users = {
   "userRandomID": {
     id: "userRandomID",
@@ -33,7 +42,13 @@ const users = {
   gu6g6f:
    { id: 'gu6g6f',
      email: 'isaac.smitty@gmail.com',
-     password: '12345' }
+     password: '12345'
+   },
+   f74b36:
+   { id: 'f74b36',
+     email: 'isaac@elize.ca',
+     password: '12345'
+   }
 }
 
 function checkDBForEmail(email) {
@@ -44,6 +59,16 @@ function checkDBForEmail(email) {
   }
     return false;
 };
+
+function checkDBForLogin(cookie) {
+  for (var id in users) {
+    if (users[id].id === cookie) {
+      return true;
+    }
+  }
+    return false;
+};
+
 
 app.get('/', (request, response) => {
   response.send('Hello!');
@@ -69,10 +94,9 @@ app.post('/register', (request, response) => {
     response.send ('400 : Email is already used.');
   } else {
     let id = random();
+    console.log(id);
     users[id] = {'id': id, email: request.body.email ,password: request.body.password};
     response.cookie('user_id', id);
-    //console.log(users);
-    //console.log(response.cookies['user_id']);
     response.redirect('/urls');
   }
 });
@@ -102,24 +126,31 @@ app.post('/logout', (request, response) => {
 });
 
 app.get("/urls/new", (request, response) => {
-  let templateVars = {urls: urlDB, user: users[request.cookies["user_id"]]};
-  response.render("urls_new", templateVars);
-
+  if (checkDBForLogin(request.cookies['user_id'])) {
+    let templateVars = {urls: urlDB, user: users[request.cookies["user_id"]]};
+    response.render('urls_new', templateVars);
+  } else {
+    let templateVars = {urls: urlDB, user: users[request.cookies["user_id"]]};
+    response.render('login', templateVars);
+  }
 });
 
-app.post("/urls", (request, response) => {
-  let tiny = random();
-  urlDB[tiny] = request.body.longURL;
+app.post('/urls', (request, response) => {
+  // let accessableURLs = urlsForUser(request.cookies['user_id']);
+  let shortURL = random();
+  urlDB[shortURL] = {longURL: request.body.longURL, userID: request.cookies["user_id"]};
   response.redirect('http://127.0.0.1:8080/urls/');
 });
 
 app.get('/urls', (request, response) => {
-  let templateVars = {urls: urlDB, user: users[request.cookies["user_id"]]};
+  let accessableURLs = urlsForUser(request.cookies['user_id']);
+  //console.log(accessableURLs);
+  let templateVars = {urls: accessableURLs, user: users[request.cookies["user_id"]]};
   response.render('urls_index', templateVars);
 });
 
 app.get('/u/:shortURL', (request, response) => {
-  let longURL = urlDB[request.params.shortURL];
+  let longURL = urlDB[request.params.shortURL].longURL;
   if (longURL.slice(0, 7) === 'http://') {
     response.redirect(longURL);
   } else {
@@ -133,13 +164,13 @@ app.post('/urls/:shortURL/delete', (request, response) => {
 });
 
 app.post('/urls/:shortURL', (request, response) => {
-  urlDB[request.params.shortURL] = request.body.longURL;
+  urlDB[request.params.shortURL].longURL = request.body.longURL;
   response.redirect('/urls',);
 });
 
 app.get('/urls/:id', (request, response) => {
   let templateVars = {shortURL: request.params.id,
-   longURL: urlDB[request.params.id],
+   longURL: urlDB[request.params.id].longURL,
    user: users[request.cookies["user_id"]]}
   response.render('urls_show', templateVars);
 });
