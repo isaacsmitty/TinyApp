688e806f2f21bb70f04a3fc96bc80593f7b190c3
Isaac Smith <isaac.smitty@gmail.com>
2019-02-15 20:18:01 +0000

    add encrypted cookies, changed stylesheet, add to get-/urls/:id so that on the shortURL owner can edit them, redirect reg and login pages if already logged in

--- express_server.js
+++ express_server.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: true}));
-const cookieParser = require('cookie-parser');
-app.use(cookieParser('long rambling statement'));
+const cookieSession = require('cookie-session')
 const random = require('./randomString.js');
 const bcrypt = require('bcrypt');
 
-const PORT = 8080
-;
+const PORT = 8080;
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieSession({
+  name: 'session',
+  keys: ['put that shit it everything'],
+}));
 
 const urlDB = {
   'b2xVn2': {longURL: 'http://www.lighthouselabs.ca', userID: 'f74b36'},
@@ -42,12 +44,12 @@ const users = {
   gu6g6f:
    { id: 'gu6g6f',
      email: 'isaac.smitty@gmail.com',
-     password: '12345'
+     password: '$2b$10$U7OBGTxkBi2msVn3MNsu2eoHgf05CaGUmKnBTK0wabXcmYchDXMbu'
    },
    f74b36:
    { id: 'f74b36',
      email: 'isaac@elize.ca',
-     password: '12345'
+     password: '$2b$10$xkteF6yAD9staQbm/pj6Pe3SJCJ.pOLKQ3DBCNrXAkdy/JV7pAQOm'
    }
 }
 
@@ -71,87 +73,86 @@ function checkDBForLogin(cookie) {
 
 
 app.get('/', (request, response) => {
-  response.send('Hello!');
+  if (request.session.user_id) {
+    response.redirect('/urls')
+  } else {
+    response.redirect('/login')
+  };
 });
 
 app.get('/urls.json', (request, response) => {
   response.json(urlDB);
 });
 
-app.get('/hello', (request, response) => {
-  response.send('<html><body>Hello <b>World</b></body></html>')
-});
-
 app.get('/register', (request, response) => {
-  let templateVars = {user: users[request.cookies["user_id"]]}
+  if (request.session.user_id) {
+    response.redirect('/urls')
+  } else {
+  let templateVars = {user: users[request.session.user_id]}
   response.render('registration', templateVars);
+  }
 });
 
 app.post('/register', (request, response) => {
   if (request.body.email === '' || request.body.password === '') {
-    response.send ('400 : invalid email or password');
+    response.status(400).send('<html><body><h1><b>Invalid Email or Password. Please try again.</b></1></body></html>');
   } else if (checkDBForEmail(request.body.email)) {
-    response.send ('400 : Email is already used.');
+    response.status(400).send('<html><body><h1><b>Email is already registered. Please try again.</b></1></body></html>');
   } else {
     let hash = bcrypt.hashSync(request.body.password, 10);
-    //console.log(hash);
+    console.log(hash);
     let id = random();
     users[id] = {'id': id, email: request.body.email, password: hash};
-    console.log(users[id].password);
-    response.cookie('user_id', id);
+    request.session.user_id = id;
     response.redirect('/urls');
   }
 });
 
 app.get('/login', (request, response) => {
-  let templateVars = {user: users[request.cookies["user_id"]]}
-  response.render('login', templateVars);
-});
-
-app.post('/login', (request, response) => {
-
-  const objectID = checkDBForEmail(request.body.email)
-  // let unhash = bcrypt.compareSync(request.body.password, objectID.password)
-  // console.log(unhash);
-
-  if (!objectID) {
-    response.send ('403 : Invalid Email');
-  } else if (bcrypt.compareSync(request.body.password, objectID.password)) {
-    response.cookie('user_id', objectID.id);
-    response.redirect('/urls');
+  if (request.session.user_id) {
+    response.redirect('/urls')
   } else {
-    response.send ('403 : Invalid Password')
+  let templateVars = {user: users[request.session.user_id]}
+  response.render('login', templateVars);
   }
 });
 
-app.post('/logout', (request, response) => {
-  response.clearCookie('user_id');
-  response.redirect('/urls',);
-
-});
-
-app.get("/urls/new", (request, response) => {
-  if (checkDBForLogin(request.cookies['user_id'])) {
-    let templateVars = {urls: urlDB, user: users[request.cookies["user_id"]]};
-    response.render('urls_new', templateVars);
+app.post('/login', (request, response) => {
+  const objectID = checkDBForEmail(request.body.email)
+  if (!objectID) {
+    response.send ('403 : Invalid Email');
+  } else if (bcrypt.compareSync(request.body.password, objectID.password)) {
+    request.session.user_id = objectID.id;
+    response.redirect('/urls');
   } else {
-    let templateVars = {urls: urlDB, user: users[request.cookies["user_id"]]};
-    response.render('login', templateVars);
+    response.status(403).send('<html><body><h1><b>Invalid password. Please try again.</b></1></body></html>');
   }
 });
 
 app.post('/urls', (request, response) => {
-  // let accessableURLs = urlsForUser(request.cookies['user_id']);
   let shortURL = random();
-  urlDB[shortURL] = {longURL: request.body.longURL, userID: request.cookies["user_id"]};
-  response.redirect('http://127.0.0.1:8080/urls/');
+  urlDB[shortURL] = {longURL: request.body.longURL, userID: request.session.user_id};
+  response.redirect('/urls');
 });
 
 app.get('/urls', (request, response) => {
-  let accessableURLs = urlsForUser(request.cookies['user_id']);
-  //console.log(accessableURLs);
-  let templateVars = {urls: accessableURLs, user: users[request.cookies["user_id"]]};
-  response.render('urls_index', templateVars);
+  if (request.session.user_id) {
+    let accessableURLs = urlsForUser(request.session.user_id);
+    let templateVars = {urls: accessableURLs, user: users[request.session.user_id]};
+    response.render('urls_index', templateVars);
+  } else {
+    response.redirect('/login');
+  }
+});
+
+app.get("/urls/new", (request, response) => {
+  if (checkDBForLogin(request.session.user_id)) {
+    let templateVars = {urls: urlDB, user: users[request.session.user_id]};
+    response.render('urls_new', templateVars);
+  } else {
+    let templateVars = {urls: urlDB, user: users[request.session.user_id]};
+    response.render('login', templateVars);
+  }
 });
 
 app.get('/u/:shortURL', (request, response) => {
@@ -163,21 +164,35 @@ app.get('/u/:shortURL', (request, response) => {
   }
 });
 
-app.post('/urls/:shortURL/delete', (request, response) => {
-  delete urlDB[request.params.shortURL];
-  response.redirect('/urls',);
-});
-
 app.post('/urls/:shortURL', (request, response) => {
   urlDB[request.params.shortURL].longURL = request.body.longURL;
   response.redirect('/urls',);
 });
 
+app.post('/urls/:shortURL/delete', (request, response) => {
+  delete urlDB[request.params.shortURL];
+  response.redirect('/urls',);
+});
+
 app.get('/urls/:id', (request, response) => {
-  let templateVars = {shortURL: request.params.id,
-   longURL: urlDB[request.params.id].longURL,
-   user: users[request.cookies["user_id"]]}
-  response.render('urls_show', templateVars);
+  if (request.session.user_id === urlDB[request.params.id].userID) {
+    let templateVars = {
+      shortURL : request.params.id,
+      longURL  : urlDB[request.params.id].longURL,
+      user     : users[request.session.user_id]
+    }
+    response.render('urls_show', templateVars);
+  } else if (request.session.user_id) {
+      response.redirect('/urls');
+  } else {
+      response.redirect('/login');
+  }
+});
+
+app.post('/logout', (request, response) => {
+  request.session = null;
+  response.redirect('/urls');
+
 });
 
 app.listen(PORT, () => {
